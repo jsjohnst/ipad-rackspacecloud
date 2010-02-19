11af52a7e3df68f466a4bfd5bea26c0baece7fa5
Michael Mayo <greenisus@gmail.com>
2010-02-18 17:29:57 -0800

    some more rubyisms on NSString

--- Classes/NSString+Rubyisms.h
+++ Classes/NSString+Rubyisms.h
@@ -13,4 +13,17 @@
 // Returns a copy of str with the first character converted to uppercase and the remainder to lowercase.
 -(NSString *)capitalize;
 
+-(NSString *)downcase;
+-(NSString *)upcase;
+
+-(NSArray *)split;
+-(NSArray *)split:(NSString *)str;
+
+-(NSString *)camelize;
+-(NSString *)camelize:(BOOL)firstCharacterUppercase;
+
+-(Class)constantize;
+
+-(NSString *)dasherize;
+
 @end
--- Classes/NSString+Rubyisms.m
+++ Classes/NSString+Rubyisms.m
@@ -9,9 +9,47 @@
 @implementation NSString (Rubyisms)
 
 -(NSString *)capitalize {
-    NSMutableArray *components = [NSMutableArray arrayWithArray:[self componentsSeparatedByString:@" "]];
+    NSMutableArray *components = [NSMutableArray arrayWithArray:[self split]];
     [components replaceObjectAtIndex:0 withObject:[[components objectAtIndex:0] capitalizedString]];
     return [components componentsJoinedByString:@" "];
+}
+
+-(NSString *)downcase {
+    return [self lowercaseString];
+}
+
+-(NSString *)upcase {
+    return [self uppercaseString];
+}
+
+-(NSArray *)split {
+    return [self split:@" "];
+}
+
+-(NSArray *)split:(NSString *)str {
+    return [self componentsSeparatedByString:str];
+}
+
+-(NSString *)camelize {
+    return [self camelize:YES];
+}
+
+-(NSString *)camelize:(BOOL)firstCharacterUppercase {
+    NSMutableArray *components = [NSMutableArray arrayWithArray:[self split:@"_"]];
+    NSUInteger i = firstCharacterUppercase ? 0 : 1;
+    while (i < [components count]) {
+        [components replaceObjectAtIndex:i withObject:[[components objectAtIndex:i] capitalizedString]];
+        i++;
+    }
+    return [components componentsJoinedByString:@""];
+}
+
+-(Class)constantize {
+    return NSClassFromString(self);
+}
+
+-(NSString *)dasherize {
+    return [self stringByReplacingOccurrencesOfString:@"_" withString:@"-"];
 }
 
 @end
